a2f9e39c5bb0d843fd8d6460304b03cc5e8afcf5
Salvador Montiel <salvadormontiel8@gmail.com>
2023-09-24 21:32:34 -0600

    :recycle: Refactor useState to arrow function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { readonly, Ref, ref } from 'vue';
 
-export default function useState<T>(initialState: T) {
+const useState = <T>(initialState: T) => {
     const state = ref<T>(initialState) as Ref<T>;
     const setState = (newState: T) => {
         state.value = newState;
     };
 
     return [readonly(state), setState];
-}
+};
+
+export default useState;
